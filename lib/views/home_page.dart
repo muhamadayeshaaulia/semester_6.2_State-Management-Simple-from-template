c0955c386696da3cea4b71a5c5b4e6bb670b8821
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-02-20 15:05:01 +0700

    memberikan space pada button dengan sizedbox

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -78,6 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 tooltip: 'Decrement',
                 child: const Icon(Icons.remove),
               ),
+              SizedBox(width: 10),
               FloatingActionButton(
                 onPressed: () {
                   value.increment();
